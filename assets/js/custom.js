846356da46e9e9a4f8db57cd13ea99384354f7a5
Varun Khandelwal <varun.k@chapter247.com>
2026-08-07 13:10:59 +0530

    feat(home): complete hero section UI and add theme and featured icons

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,8 +4,8 @@ $(".hamburgerMenu").click(function(){
   $(".headerNav").toggleClass('show');
 });
 // ----------header-Toggle-Menu----------
-// ----------AOS scroll animations----------
 
+// ----------AOS scroll animations----------
 AOS.init();
 AOS.init({
     disable: function () {
@@ -14,4 +14,45 @@ AOS.init({
     }
 });
 // ----------AOS scroll animations----------
+
+// ----------dark-themeToggle-js-start-----------
+    $(".themeToggle").on("click", function () {
+        $("html").toggleClass("dark");
+        const isDark = $("html").hasClass("dark");
+        localStorage.setItem("theme", isDark ? "dark" : "light");
+        $(".themeToggle").toggleClass("dark", isDark);
+    });
+    // Load Saved Theme
+    const savedTheme = localStorage.getItem("theme");
+    if (
+        savedTheme === "dark" ||
+        (!savedTheme && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+        $("html").addClass("dark");
+        $(".themeToggle").addClass("dark");
+    } else {
+        $("html").removeClass("dark");
+        $(".themeToggle").removeClass("dark");
+    }
+// theme-saved-js-end
+// ----------dark-themeToggle-js-end-------------
+
+// ---------Owl-Carousel-js-satrt------------
+$('.hero-slider').owlCarousel({
+        items:1,
+        loop:true,
+        margin:0,
+        nav:false,
+        dots:true,
+        autoplay: true,
+        mouseDrag: false,
+        autoplayTimeout: 3500,
+        autoplaySpeed: 3500,
+        autoplayHoverPause:true,
+        animateOut: "fadeOut",
+        animateIn: "fadeIn",
+        autoHeight:true
+    })
+// ---------Owl-Carousel-js-end------------
+
 
